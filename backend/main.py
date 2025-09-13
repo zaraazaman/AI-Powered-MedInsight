adb6f2b3d03bcfeb4cb500666f548c61c78ae8cf
Zara <168398928+zaraazaman@users.noreply.github.com>
2025-09-14 00:40:01 +0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,8 @@
 import os
 import sys
-
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-
 from frontend.ui_gradio import user_interface
+
 
 if __name__ == "__main__":
     app = user_interface()
